9b99155e6695d7215ac194bbb0c94b32e180399d
sr3devop <sr3devops@gmail.com>
2021-01-24 12:37:09 +0530

    404 feature is added

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/error', (req, res) => {
+    console.log(res)
+  res.send('404')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+}) 
